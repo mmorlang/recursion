01106b8465eafab40d7d8a6876736c412975dbde
Mickie Morlang <mmorlang@gmail.com>
2020-06-10 07:54:18 -0600

    Javadocs

--- src/edu/cnm/deepdive/Palindromes.java
+++ src/edu/cnm/deepdive/Palindromes.java
@@ -4,15 +4,15 @@ public class Palindromes {
 
   public static boolean testRecursive(String input) {
 
-    return input.length() <= 1
+    /** @return command returns the length of the current string after it has been evaluated by its conditions.*/
+
+     return input.length() <= 1
         || (
         input.charAt(0) == input.charAt(input.length() - 1)
             && testRecursive(input.substring(1, input.length() - 1))
     );
   }
 
-  public static boolean testDenormalized(String string) {
+/** @testDenormalized modifies current string by removing all white space and all uppercase */
+/** @return returns a string that has been modified so that it may be processed by the testRecursive method*/
 
-   String.removeAll = new String();
-
-}
